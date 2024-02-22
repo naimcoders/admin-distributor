e3127c2e8fc9def1bea3c4138fcfe43c09e932bc
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-02-22 15:12:19 +0800

    add: header price modal

--- src/layouts/product/Modals/Price.tsx
+++ src/layouts/product/Modals/Price.tsx
@@ -1,11 +1,27 @@
+import { PencilIcon } from "@heroicons/react/24/outline";
 import { Modal } from "src/components/Modal";
 import { useActiveModal } from "src/stores/modalStore";
+import { IconColor } from "src/types";
 
 const PriceModal = () => {
   const { isPrice, actionIsPrice } = useActiveModal();
   return (
     <Modal isOpen={isPrice} closeModal={actionIsPrice}>
-      <main className="my-4 flexcol gap-6">Lorem, ipsum dolor.</main>
+      <main className="my-4 flexcol gap-5">
+        <header className="flex gap-4 justify-between">
+          <h1 className="capitalize font-semibold">
+            Atur harga variasi produk
+          </h1>
+          <button
+            className={`text-[${IconColor.red}] text-sm capitalize hover:text-blue-500 inline-flex gap-1 items-center`}
+            title="Edit secara massal"
+          >
+            <PencilIcon width={16} />
+            massal
+          </button>
+        </header>
+        <hr />
+      </main>
     </Modal>
   );
 };

855bd405763638f9d164cc96f0fe32728a111897
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-03-04 20:36:06 +0800

    refactor: login path

--- src/api/login.service.ts
+++ src/api/login.service.ts
@@ -24,7 +24,7 @@ class Api {
     return Api.instance;
   }
 
-  private path = "login-admin";
+  private path = "auth/login-admin";
 
   async login(r: ReqLogin): Promise<void> {
     const token = await req<string>({

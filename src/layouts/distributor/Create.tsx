9a86af7b8d587a7f392b176a3695f6a21baa0948
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-03-23 19:29:55 +0800

    fix

--- src/layouts/distributor/Create.tsx
+++ src/layouts/distributor/Create.tsx
@@ -19,7 +19,7 @@ import { IconColor } from "src/types";
 import { CoordinateModal, UserCoordinate } from "src/components/Coordinate";
 import useGeneralStore from "src/stores/generalStore";
 import { useLocation } from "src/api/location.service";
-import { Chip } from "@nextui-org/react";
+import { Chip, CircularProgress } from "@nextui-org/react";
 
 interface DefaultValues {
   ownerName: string;
@@ -37,23 +37,181 @@ interface DefaultValues {
 }
 
 const Create = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<DefaultValues>();
-
-  const { ktpBlob, ktpRef, onClick, onChange, setKtpBlob } = useKtp();
   const coordinate = useGeneralStore((v) => v.coordinate);
+  const { fields } = useField();
   const { actionIsCoordinate } = useActiveModal();
+  const { forms, geoLocation, zipCode, onSubmit } = useApi();
 
-  const onSubmit = handleSubmit(async (e) => {
-    console.log(e);
-  });
+  return (
+    <main className="flexcol gap-5 lg:gap-8">
+      {!coordinate ? (
+        <Textfield
+          name="coodinate"
+          defaultValue=""
+          label="koordinat usaha"
+          placeholder="tentukan koordinat"
+          control={forms.control}
+          onClick={actionIsCoordinate}
+          startContent={<MapPinIcon width={16} color={IconColor.zinc} />}
+          errorMessage={handleErrorMessage(
+            forms.formState.errors,
+            "coordinate"
+          )}
+          readOnly={{ isValue: true, cursor: "cursor-pointer" }}
+          rules={{
+            required: { value: true, message: "pilih koordinat" },
+          }}
+        />
+      ) : (
+        <section className="flex gap-8">
+          <section className="aspect-video lg:w-[20rem] w-[10rem]">
+            <UserCoordinate
+              label="koordinat usaha"
+              lat={coordinate.lat}
+              lng={coordinate.lng}
+              onClick={actionIsCoordinate}
+            />
+          </section>
+          {!geoLocation.data ? (
+            <CircularProgress
+              size="md"
+              aria-label="loading..."
+              className="mx-auto"
+            />
+          ) : (
+            <div className="flexcol gap-2">
+              <Chip color="default">{geoLocation.data?.addressName}</Chip>
+              <Chip color="default">
+                {geoLocation.data?.district} / {zipCode}
+              </Chip>
+            </div>
+          )}
+        </section>
+      )}
+
+      {coordinate && (
+        <>
+          <section className="grid lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-8">
+            {fields.map((v, idx) => (
+              <React.Fragment key={idx}>
+                {["text", "number", "email"].includes(v.type!) && (
+                  <Textfield
+                    {...v}
+                    control={forms.control}
+                    defaultValue=""
+                    autoComplete={v.autoComplete}
+                    errorMessage={handleErrorMessage(
+                      forms.formState.errors,
+                      v.name
+                    )}
+                    rules={{
+                      required: { value: true, message: v.errorMessage! },
+                    }}
+                  />
+                )}
+
+                {["modal"].includes(v.type!) && (
+                  <Textfield
+                    {...v}
+                    control={forms.control}
+                    defaultValue=""
+                    autoComplete={v.autoComplete}
+                    endContent={
+                      <ChevronRightIcon width={16} color={IconColor.zinc} />
+                    }
+                    errorMessage={handleErrorMessage(
+                      forms.formState.errors,
+                      v.name
+                    )}
+                    readOnly={{ isValue: true, cursor: "cursor-pointer" }}
+                    rules={{
+                      required: { value: true, message: v.errorMessage! },
+                    }}
+                  />
+                )}
+
+                {["file"].includes(v.type!) &&
+                  (!v.defaultValue ? (
+                    <File
+                      name={v.name}
+                      label={v.label}
+                      control={forms.control}
+                      placeholder={v.placeholder}
+                      ref={v.uploadImage?.file.ref}
+                      onClick={v.uploadImage?.file.onClick}
+                      onChange={v.uploadImage?.file.onChange}
+                      errorMessage={handleErrorMessage(
+                        forms.formState.errors,
+                        v.name
+                      )}
+                      startContent={
+                        <ArrowUpTrayIcon width={16} color={IconColor.zinc} />
+                      }
+                      rules={{
+                        required: { value: true, message: v.errorMessage! },
+                      }}
+                    />
+                  ) : (
+                    <LabelAndImage
+                      src={v.defaultValue}
+                      label={v.label}
+                      actions={v.uploadImage?.image.actions}
+                    />
+                  ))}
+              </React.Fragment>
+            ))}
+          </section>
+
+          <Button
+            aria-label="simpan"
+            onClick={onSubmit}
+            className="mx-auto lg:mt-12 mt-6"
+          />
+        </>
+      )}
+
+      <CoordinateModal />
+    </main>
+  );
+};
+
+const useApi = () => {
+  const forms = useForm<DefaultValues>();
+  const coordinate = useGeneralStore((v) => v.coordinate);
 
   const { findGeoLocation } = useLocation();
   const geoLocation = findGeoLocation(coordinate?.lat!, coordinate?.lng!);
   const zipCode = geoLocation.data?.zipCode ? geoLocation.data?.zipCode : "-";
+
+  const onSubmit = forms.handleSubmit(async (e) => {
+    console.log(e);
+  });
+
+  return { zipCode, geoLocation, forms, onSubmit };
+};
+
+export const useKtp = () => {
+  const [ktpBlob, setKtpBlob] = React.useState("");
+  const ktpRef = React.useRef<ChildRef>(null);
+
+  const onClick = () => {
+    if (ktpRef.current) ktpRef.current.click();
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return null;
+    const blob = URL.createObjectURL(files[0]);
+    setKtpBlob(blob);
+  };
+
+  return { ktpBlob, ktpRef, onClick, onChange, setKtpBlob };
+};
+
+export default Create;
+
+const useField = () => {
+  const { ktpBlob, ktpRef, onClick, onChange, setKtpBlob } = useKtp();
 
   const fields: TextfieldProps[] = [
     objectFields({
@@ -108,133 +266,5 @@ const Create = () => {
     }),
   ];
 
-  return (
-    <main className="flexcol gap-5 lg:gap-8">
-      {!coordinate ? (
-        <Textfield
-          name="coodinate"
-          defaultValue=""
-          label="koordinat usaha"
-          placeholder="tentukan koordinat"
-          control={control}
-          onClick={actionIsCoordinate}
-          startContent={<MapPinIcon width={16} color={IconColor.zinc} />}
-          errorMessage={handleErrorMessage(errors, "coordinate")}
-          readOnly={{ isValue: true, cursor: "cursor-pointer" }}
-          rules={{
-            required: { value: true, message: "pilih koordinat" },
-          }}
-        />
-      ) : (
-        <section className="flex gap-8">
-          <section className="aspect-video lg:w-[20rem] w-[10rem]">
-            <UserCoordinate
-              label="koordinat usaha"
-              lat={coordinate.lat}
-              lng={coordinate.lng}
-              onClick={actionIsCoordinate}
-            />
-          </section>
-          <div className="flexcol gap-2">
-            <Chip color="default">{geoLocation.data?.addressName}</Chip>
-            <Chip color="default">
-              {geoLocation.data?.district} / {zipCode}
-            </Chip>
-          </div>
-        </section>
-      )}
-
-      <>
-        <section className="grid lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-8">
-          {fields.map((v, idx) => (
-            <React.Fragment key={idx}>
-              {["text", "number", "email"].includes(v.type!) && (
-                <Textfield
-                  {...v}
-                  control={control}
-                  defaultValue={v.defaultValue}
-                  autoComplete={v.autoComplete}
-                  errorMessage={handleErrorMessage(errors, v.name)}
-                  rules={{
-                    required: { value: true, message: v.errorMessage! },
-                  }}
-                />
-              )}
-
-              {["modal"].includes(v.type!) && (
-                <Textfield
-                  {...v}
-                  control={control}
-                  defaultValue={v.defaultValue}
-                  autoComplete={v.autoComplete}
-                  endContent={
-                    <ChevronRightIcon width={16} color={IconColor.zinc} />
-                  }
-                  errorMessage={handleErrorMessage(errors, v.name)}
-                  readOnly={{ isValue: true, cursor: "cursor-pointer" }}
-                  rules={{
-                    required: { value: true, message: v.errorMessage! },
-                  }}
-                />
-              )}
-
-              {["file"].includes(v.type!) &&
-                (!v.defaultValue ? (
-                  <File
-                    name={v.name}
-                    label={v.label}
-                    control={control}
-                    placeholder={v.placeholder}
-                    ref={v.uploadImage?.file.ref}
-                    onClick={v.uploadImage?.file.onClick}
-                    onChange={v.uploadImage?.file.onChange}
-                    errorMessage={handleErrorMessage(errors, v.name)}
-                    startContent={
-                      <ArrowUpTrayIcon width={16} color={IconColor.zinc} />
-                    }
-                    rules={{
-                      required: { value: true, message: v.errorMessage! },
-                    }}
-                  />
-                ) : (
-                  <LabelAndImage
-                    src={v.defaultValue}
-                    label={v.label}
-                    actions={v.uploadImage?.image.actions}
-                  />
-                ))}
-            </React.Fragment>
-          ))}
-        </section>
-
-        <Button
-          aria-label="simpan"
-          onClick={onSubmit}
-          className="mx-auto lg:mt-12 mt-6"
-        />
-      </>
-
-      <CoordinateModal />
-    </main>
-  );
+  return { fields };
 };
-
-export const useKtp = () => {
-  const [ktpBlob, setKtpBlob] = React.useState("");
-  const ktpRef = React.useRef<ChildRef>(null);
-
-  const onClick = () => {
-    if (ktpRef.current) ktpRef.current.click();
-  };
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (!files) return null;
-    const blob = URL.createObjectURL(files[0]);
-    setKtpBlob(blob);
-  };
-
-  return { ktpBlob, ktpRef, onClick, onChange, setKtpBlob };
-};
-
-export default Create;

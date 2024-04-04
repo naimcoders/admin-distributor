a5930535e374d60056a54645e2644a9499b7585d
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-04-04 23:52:30 +0800

    fix: fcmToken

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import cx from "classnames";
 import mokes from "src/assets/images/mokes.png";
 import Image from "src/components/Image";
 import { FieldValues, useForm } from "react-hook-form";
@@ -9,7 +10,7 @@ import {
   objectFields,
 } from "src/components/Textfield";
 import { handleErrorMessage } from "src/helpers";
-import { KeyboardEvent, useEffect, useState } from "react";
+import React from "react";
 import { requestForToken } from "src/firebase";
 import { useLogin as useLoginApi } from "src/api/login.service";
 import { useNavigate } from "react-router-dom";
@@ -17,11 +18,22 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 import { IconColor } from "src/types";
 
 const LoginPage = () => {
+  const { notificationPermission } = useHook();
+
   return (
     <main className="bg-primary min-h-screen flex flex-col justify-between">
       <section className="md:px-12 py-8 flex flex-col gap-10 items-center md:flex-row justify-center md:gap-28 lg:gap-56 my-auto">
         <Image src={mokes} alt="Mokes" width={250} loading="lazy" />
-        <section className="bg-secondary p-2 rounded-2xl flex flex-col gap-10 px-6 py-10 w-3/4 sm:max-w-[28rem]">
+        <section className="bg-secondary p-2 rounded-2xl flex flex-col gap-10 px-6 py-10 w-3/4 sm:max-w-[28rem] relative">
+          <span
+            className={cx(
+              "w-3 h-3 rounded-full absolute top-2 right-2",
+              notificationPermission !== "granted"
+                ? "bg-red-500"
+                : "bg-green-500"
+            )}
+          ></span>
+
           <header>
             <h1 className="font-interBold capitalize text-lg sm:text-xl md:text-2xl">
               login admin distributor
@@ -38,7 +50,7 @@ const LoginPage = () => {
 };
 
 export const useLoginFields = () => {
-  const [isPassword, setIsPassword] = useState(false);
+  const [isPassword, setIsPassword] = React.useState(false);
 
   const logins: TextfieldProps[] = [
     objectFields({
@@ -92,7 +104,7 @@ const Form = () => {
   const { logins } = useLoginFields();
 
   return (
-    <section className="flexcol gap-8">
+    <section className="flex flex-col gap-8">
       {logins.map((el, idx) => (
         <Textfield
           {...el}
@@ -116,34 +128,68 @@ const Form = () => {
 };
 
 const useHook = () => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = React.useState("");
+  const [notificationPermission, setNotificationPermission] =
+    React.useState("default");
 
-  useEffect(() => {
-    requestForToken().then((token) => {
-      setToken(token);
-    });
-  }, []);
+  const notificationStatus: string[] = ["default", "denied"];
+
+  React.useEffect(() => {
+    if (notificationStatus.includes(notificationPermission)) {
+      onNotificationPermission();
+    } else requestToken();
+  }, [notificationPermission]);
 
   const {
     control,
     handleSubmit,
     getValues,
     formState: { errors },
-  } = useForm<FieldValues>({ mode: "onChange" });
+  } = useForm<FieldValues>();
 
   const { mutateAsync, isPending } = useLoginApi();
   const navigate = useNavigate();
 
+  const requestToken = async () => {
+    try {
+      toast.loading("Memuat token...", {
+        toastId: "token",
+      });
+      const getToken = await requestForToken();
+      setToken(getToken);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      toast.dismiss("token");
+    }
+  };
+
+  const onNotificationPermission = async () => {
+    if (!("Notification" in window)) {
+      console.error("Browser tidak mendukung notifikasi");
+      return;
+    }
+
+    const permission = await Notification.requestPermission();
+    setNotificationPermission(permission);
+  };
+
   const onSubmit = handleSubmit(async (e) => {
     try {
-      const result = {
-        email: e.email,
-        password: e.password,
-        fcmToken: token ? token : "-",
-      };
+      if (notificationStatus.includes(notificationPermission)) {
+        toast.error("Silakan izinkan notifikasi untuk login");
+      }
 
-      await mutateAsync(result);
-      navigate("/dashboard");
+      if (token) {
+        const result = {
+          email: e.email,
+          password: e.password,
+          fcmToken: token,
+        };
+
+        await mutateAsync(result);
+        navigate("/dashboard");
+      }
     } catch (e) {
       const error = e as Error;
       if (error.message?.includes("fcmtoken")) {
@@ -154,19 +200,25 @@ const useHook = () => {
     }
   });
 
-  const onKeyDown = async (e: KeyboardEvent<HTMLInputElement>) => {
+  const onKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     const { email, password } = getValues() as LoginFieldProps;
 
     try {
       if (e.key === "Enter") {
-        const result = {
-          email,
-          password,
-          fcmToken: token,
-        };
+        if (notificationStatus.includes(notificationPermission)) {
+          toast.error("Silakan izinkan notifikasi untuk login");
+        }
 
-        await mutateAsync(result);
-        navigate("/dashboard");
+        if (token) {
+          const result = {
+            email: email,
+            password: password,
+            fcmToken: token,
+          };
+
+          await mutateAsync(result);
+          navigate("/dashboard");
+        }
       }
     } catch (e) {
       const error = e as Error;
@@ -174,7 +226,15 @@ const useHook = () => {
     }
   };
 
-  return { onSubmit, onKeyDown, control, errors, isPending };
+  return {
+    onSubmit,
+    onKeyDown,
+    control,
+    errors,
+    isPending,
+    notificationPermission,
+    token,
+  };
 };
 
 export default LoginPage;

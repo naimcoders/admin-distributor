7376dc2b246fd03c2dd57c2a30308501f1b29ac3
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-04-05 21:53:07 +0800

    add: activate account api

--- src/api/pilipay.service.ts
+++ src/api/pilipay.service.ts
@@ -1,6 +1,7 @@
 import queryString from "query-string";
 import { req } from "./request";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 interface Pilipay {
   id: string;
@@ -25,6 +26,14 @@ interface History {
   createdAt: number;
   successAt: number;
   failedAt: number;
+}
+
+interface Activated {
+  lat: number;
+  lng: number;
+  otp: string;
+  otpId: string;
+  pin: string;
 }
 
 class Api {
@@ -52,10 +61,21 @@ class Api {
       errors: "",
     });
   }
+
+  async activated(r: Activated): Promise<Pilipay> {
+    return await req<Pilipay>({
+      method: "POST",
+      isNoAuth: false,
+      path: `${this.path}/activated`,
+      errors: "",
+      body: r,
+    });
+  }
 }
 
 interface ApiPilipayInfo {
   findMeWallet(showHistory: boolean): Promise<Pilipay>;
+  activated(r: Activated): Promise<Pilipay>;
 }
 
 function getPilipayApiInfo(): ApiPilipayInfo {
@@ -73,5 +93,14 @@ export const usePilipay = () => {
     });
   };
 
-  return { findMeWallet };
+  const activated = useMutation<Pilipay, Error, { data: Activated }>({
+    mutationKey: [key],
+    mutationFn: async (r) => await getPilipayApiInfo().activated(r.data),
+    onSuccess: () => {
+      toast.success("Akun berhasil diaktifkan");
+    },
+    onError: (e) => toast.error(e.message),
+  });
+
+  return { findMeWallet, activated };
 };

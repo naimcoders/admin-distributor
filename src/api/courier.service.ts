e0a91113fde1a19ae70a7c18dfc2d506268fd2b5
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-05-01 17:08:43 +0800

    modify: api

--- src/api/courier.service.ts
+++ src/api/courier.service.ts
@@ -130,5 +130,5 @@ export const createCourierInternal = () => {
     },
   });
 
-  return { mutateAsync, isLoadingCreateCourier: isPending };
+  return { mutateAsync, isPending };
 };
--- src/layouts/account/CourierInternal.tsx
+++ src/layouts/account/CourierInternal.tsx
@@ -4,7 +4,10 @@ import {
   objectFields,
 } from "src/components/Textfield";
 import Template from "./Template";
-import { ReqCourierInternal, useCourier } from "src/api/courier.service";
+import {
+  ReqCourierInternal,
+  createCourierInternal,
+} from "src/api/courier.service";
 import { useForm } from "react-hook-form";
 import { handleErrorMessage } from "src/helpers";
 import { toast } from "react-toastify";
@@ -49,14 +52,15 @@ const useApi = () => {
     formState: { errors },
   } = useForm<ReqCourierInternal>();
 
-  const { createCourierInternal } = useCourier();
+  // const { createCourierInternal } = useCourier();
+  const createCourier = createCourierInternal();
   const { data } = findLocationByUserId();
 
   const onSubmit = handleSubmit(async (e) => {
     try {
       if (!data) return;
 
-      await createCourierInternal.mutateAsync({
+      await createCourier.mutateAsync({
         email: e.email,
         name: e.name,
         phoneNumber: e.phoneNumber,
@@ -81,7 +85,7 @@ const useApi = () => {
           location: data[0],
         };
 
-        await createCourierInternal.mutateAsync(obj);
+        await createCourier.mutateAsync(obj);
         toast.success("Kurir internal berhasil dibuat");
         reset();
       } catch (e) {
@@ -95,7 +99,7 @@ const useApi = () => {
     control,
     errors,
     onSubmit,
-    isPending: createCourierInternal.isPending,
+    isPending: createCourier.isPending,
     onSubmitKeyDown,
   };
 };

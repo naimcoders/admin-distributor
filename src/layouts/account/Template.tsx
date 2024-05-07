514ef0c4175099e0ddd7e3fdac2feb3281ac5268
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-05-07 18:53:48 +0800

    fix

--- src/layouts/account/Template.tsx
+++ src/layouts/account/Template.tsx
@@ -1,9 +1,11 @@
+import cx from "classnames";
 import { Button } from "src/components/Button";
 
 interface TemplateProps {
   title: string;
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
   btnLabelForm?: string | React.ReactNode;
 }
 
@@ -12,16 +14,19 @@ const Template: React.FC<TemplateProps> = ({
   onClick,
   title,
   btnLabelForm,
+  className,
 }) => {
   return (
-    <main className="p-5 bg-white rounded-lg max-w-[24rem] flex flex-col gap-6 mt-5">
+    <main
+      className={cx(
+        "p-5 bg-white rounded-lg max-w-[24rem] flex flex-col gap-6 mt-5",
+        className
+      )}
+    >
       <h1 className="font-bold text-xl">{title}</h1>
-
       <section className="flex flex-col gap-7">{children}</section>
 
-      <div className="flex justify-center">
-        <Button label={btnLabelForm} onClick={onClick} />
-      </div>
+      <Button label={btnLabelForm} onClick={onClick} className="mx-auto" />
     </main>
   );
 };

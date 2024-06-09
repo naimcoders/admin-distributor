986e1e4dc12305d6eea337c06bf6bbcf12073cf8
Naim Shihab <92425371+naimshihab21@users.noreply.github.com>
2024-06-09 18:23:18 +0800

    fix

--- src/layouts/sales/Create.tsx
+++ src/layouts/sales/Create.tsx
@@ -3,126 +3,163 @@ import {
   HiOutlineChevronRight,
   HiOutlineTrash,
 } from "react-icons/hi2";
-import { ChangeEvent, Fragment, useRef, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "src/components/Button";
 import { ChildRef, File, LabelAndImage } from "src/components/File";
 import { Modal } from "src/components/Modal";
-import {
-  Textfield,
-  TextfieldProps,
-  objectFields,
-} from "src/components/Textfield";
+import { Textfield } from "src/components/Textfield";
 import { useActiveModal } from "src/stores/modalStore";
 import { IconColor, UseForm } from "src/types";
-import { handleErrorMessage } from "src/helpers";
+import { handleErrorMessage, setRequiredField } from "src/helpers";
 import { Checkbox, CheckboxGroup } from "@nextui-org/react";
-import useGeneralStore from "src/stores/generalStore";
-import { GridInput } from "../Index";
+
+interface IDefaultValues {
+  comition: string;
+  name: string;
+  phoneNumber: string;
+  email: string;
+  category: string;
+}
 
 const Create = () => {
+  const [categories, setCategories] = React.useState<string[]>([]);
+
   const {
     control,
     handleSubmit,
     setValue,
     clearErrors,
     formState: { errors },
-  } = useForm<FieldValues>({
-    mode: "onChange",
-  });
-  const { fields } = useHook();
+  } = useForm<IDefaultValues>();
 
-  const category = useGeneralStore((v) => v.category);
+  const { actionIsCategory } = useActiveModal();
+  const {
+    ktpBlob,
+    ktpRef,
+    onClick,
+    onChange,
+    setKtpBlob,
+    ktpFiles,
+    setKtpFiles,
+  } = useKtp();
 
   const onSubmit = handleSubmit(async (e) => {
     try {
-      console.log(e, category);
+      console.log(e, ktpFiles, categories);
+      setKtpFiles(undefined);
     } catch (e) {
       const error = e as Error;
-      console.log(error);
+      console.error(error);
     }
   });
 
   return (
     <main>
-      <GridInput className="mt-2">
-        {fields.map((v, idx) => (
-          <Fragment key={idx}>
-            {["text", "number", "email"].includes(v.type!) && (
-              <Textfield
-                type={v.type}
-                label={v.label}
-                control={control}
-                name={v.name ?? ""}
-                placeholder={v.placeholder}
-                defaultValue={v.defaultValue}
-                autoComplete={v.autoComplete}
-                errorMessage={handleErrorMessage(errors, v.name)}
-                rules={{
-                  required: { value: true, message: v.errorMessage ?? "" },
-                }}
-                className="w-full"
-              />
-            )}
+      <section className="grid grid-cols-4 lg:gap-8 gap-4">
+        <Textfield
+          type="text"
+          label="nama sales"
+          control={control}
+          name="name"
+          placeholder="Masukkan nama sales"
+          defaultValue=""
+          errorMessage={handleErrorMessage(errors, "name")}
+          rules={{ required: setRequiredField(true, "Masukkan nama sales") }}
+          className="w-full"
+        />
 
-            {["modal"].includes(v.type!) && (
-              <Textfield
-                name={v.name!}
-                label={v.label}
-                control={control}
-                onClick={v.onClick}
-                readOnly={v.readOnly}
-                placeholder={v.placeholder}
-                defaultValue={v.defaultValue}
-                autoComplete={v.autoComplete}
-                endContent={<HiOutlineChevronRight size={16} />}
-                errorMessage={handleErrorMessage(errors, v.name)}
-                rules={{
-                  required: { value: true, message: v.errorMessage ?? "" },
-                }}
-                className="w-full"
-              />
-            )}
+        <Textfield
+          type="number"
+          label="nomor HP"
+          control={control}
+          name="phoneNumber"
+          placeholder="Masukkan nomor HP sales"
+          defaultValue=""
+          errorMessage={handleErrorMessage(errors, "phoneNumber")}
+          rules={{ required: setRequiredField(true, "Masukkan nomor HP") }}
+          className="w-full"
+        />
 
-            {["file"].includes(v.type!) &&
-              (!v.defaultValue ? (
-                <File
-                  name={v.name}
-                  label={v.label}
-                  control={control}
-                  className="w-full"
-                  placeholder={v.placeholder}
-                  ref={v.uploadImage?.file.ref}
-                  onClick={v.uploadImage?.file.onClick}
-                  onChange={v.uploadImage?.file.onChange}
-                  startContent={<HiOutlineArrowUpTray size={16} />}
-                  errorMessage={handleErrorMessage(errors, v.name)}
-                  rules={{
-                    required: { value: true, message: v.errorMessage ?? "" },
-                  }}
-                />
-              ) : (
-                <LabelAndImage src={v.defaultValue} label={v.label!} />
-              ))}
-          </Fragment>
-        ))}
-      </GridInput>
+        <Textfield
+          type="email"
+          label="email"
+          control={control}
+          name="email"
+          autoComplete="on"
+          placeholder="Masukkan alamat email"
+          defaultValue=""
+          errorMessage={handleErrorMessage(errors, "email")}
+          rules={{ required: setRequiredField(true, "masukkan email") }}
+          className="w-full"
+        />
 
-      <div className="flex justify-center mt-10">
-        <Button label="simpan" onClick={onSubmit} />
-      </div>
+        <Textfield
+          label="kategori"
+          control={control}
+          name="category"
+          placeholder="pilih kategori sales"
+          defaultValue=""
+          errorMessage={handleErrorMessage(errors, "category")}
+          rules={{ required: setRequiredField(true, "pilih kategori") }}
+          className="w-full"
+          readOnly={{ isValue: true, cursor: "cursor-pointer" }}
+          onClick={actionIsCategory}
+          endContent={<HiOutlineChevronRight size={16} />}
+        />
 
-      <CategoryModal setValue={setValue} clearErrors={clearErrors} />
+        {!ktpBlob ? (
+          <File
+            name="ktp"
+            label="KTP"
+            control={control}
+            placeholder="unggah KTP"
+            ref={ktpRef}
+            onClick={onClick}
+            onChange={onChange}
+            startContent={<HiOutlineArrowUpTray size={16} />}
+          />
+        ) : (
+          <LabelAndImage
+            src={ktpBlob}
+            label="KTP"
+            actions={[
+              {
+                src: <HiOutlineTrash size={16} color={IconColor.red} />,
+                onClick: () => setKtpBlob(""),
+              },
+            ]}
+          />
+        )}
+      </section>
+
+      <Button
+        label="simpan"
+        onClick={onSubmit}
+        className="mx-auto block mt-14"
+      />
+
+      <CategoryModal
+        setValue={setValue}
+        clearErrors={clearErrors}
+        categories={categories}
+        setCategories={setCategories}
+      />
     </main>
   );
 };
 
+interface ICategory extends Pick<UseForm, "setValue" | "clearErrors"> {
+  setCategories: React.Dispatch<React.SetStateAction<string[]>>;
+  categories: string[];
+}
+
 export const CategoryModal = ({
   setValue,
   clearErrors,
-}: Pick<UseForm, "setValue" | "clearErrors">) => {
-  const category = useGeneralStore((v) => v.category) as string[];
-  const setCategory = useGeneralStore((v) => v.setCategory);
+  categories,
+  setCategories,
+}: ICategory) => {
   const { isCategory, actionIsCategory } = useActiveModal();
 
   const data: string[] = [
@@ -133,8 +170,9 @@ export const CategoryModal = ({
   ];
 
   const key = "category";
+
   const onNext = () => {
-    setValue(key, category.join(", "));
+    setValue(key, categories.join(", "));
     clearErrors(key);
     actionIsCategory();
   };
@@ -147,8 +185,10 @@ export const CategoryModal = ({
     >
       <CheckboxGroup
         className="my-5"
-        onChange={(e) => setCategory(e)}
-        defaultValue={category}
+        // onChange={(e) => setCategories(e)}
+        // defaultValue={categories}
+        value={categories}
+        onValueChange={setCategories}
       >
         {data.map((v) => (
           <Checkbox
@@ -164,98 +204,37 @@ export const CategoryModal = ({
         ))}
       </CheckboxGroup>
 
-      <div className="flex justify-center">
-        <Button label="selanjutnya" className="mx-auto" onClick={onNext} />
-      </div>
+      <Button label="selanjutnya" className="mx-auto block" onClick={onNext} />
     </Modal>
   );
 };
 
 export const useKtp = () => {
-  const [ktpBlob, setKtpBlob] = useState("");
-  const ktpRef = useRef<ChildRef>(null);
+  const [ktpBlob, setKtpBlob] = React.useState("");
+  const [ktpFiles, setKtpFiles] = React.useState<FileList>();
+  const ktpRef = React.useRef<ChildRef>(null);
 
   const onClick = () => {
     if (ktpRef.current) ktpRef.current.click();
   };
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
-    if (!files) return null;
+    if (!files) return;
+    setKtpFiles(files);
     const blob = URL.createObjectURL(files[0]);
     setKtpBlob(blob);
   };
 
-  return { ktpBlob, ktpRef, onClick, onChange, setKtpBlob };
-};
-
-const useHook = () => {
-  const { ktpBlob, ktpRef, onClick, onChange, setKtpBlob } = useKtp();
-  const { actionIsCategory } = useActiveModal();
-
-  const fields: TextfieldProps<FieldValues>[] = [
-    objectFields({
-      label: "nama sales",
-      name: "salesName",
-      type: "text",
-      defaultValue: "",
-      autoComplete: "on",
-    }),
-    objectFields({
-      label: "nomor HP",
-      name: "phoneNumber",
-      type: "number",
-      defaultValue: "",
-      autoComplete: "on",
-    }),
-    objectFields({
-      label: "email",
-      name: "email",
-      type: "email",
-      defaultValue: "",
-      autoComplete: "on",
-    }),
-    objectFields({
-      label: "kategori",
-      name: "category",
-      type: "modal",
-      defaultValue: "",
-      placeholder: "pilih kategori sales",
-      onClick: actionIsCategory,
-      readOnly: { isValue: true, cursor: "cursor-pointer" },
-    }),
-    objectFields({
-      label: "komisi penjualan (%)",
-      name: "commission",
-      type: "number",
-      defaultValue: "",
-      placeholder: "masukkan komisi",
-    }),
-    objectFields({
-      label: "KTP sales",
-      name: "ktp",
-      type: "file",
-      placeholder: "unggah KTP",
-      defaultValue: ktpBlob,
-      uploadImage: {
-        file: {
-          ref: ktpRef,
-          onClick,
-          onChange,
-        },
-        image: {
-          actions: [
-            {
-              src: <HiOutlineTrash size={16} color={IconColor.red} />,
-              onClick: () => setKtpBlob(""),
-            },
-          ],
-        },
-      },
-    }),
-  ];
-
-  return { fields };
+  return {
+    ktpBlob,
+    ktpRef,
+    onClick,
+    onChange,
+    setKtpBlob,
+    setKtpFiles,
+    ktpFiles,
+  };
 };
 
 export default Create;
